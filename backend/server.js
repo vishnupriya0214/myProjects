17448677b4541eb600c73665d1d5de5eedf2c646
vishnupriya0214 <vishnupriya0214@gmail.com>
2025-08-03 23:19:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
+ // Railway deployment test
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 
